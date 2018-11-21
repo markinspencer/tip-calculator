fa1da349abc04aabb7c8d815dfcbbf9bbc31104a
Spencer Markin <smarkin@questica.com>
2018-11-21 11:33:41 -0500

    implement application view and calculate tip and total functionality

--- src/Update.js
+++ src/Update.js
@@ -1,7 +1,66 @@
 import * as R from "ramda";
 
-const update = (action, model) => {
-  return model;
+const ACTIONS = {
+  BILL_VALUE_INPUT: "BILL_VALUE_INPUT",
+  TIP_PERCENTAGE_INPUT: "TIP_PERCENTAGE_INPUT"
 };
+
+export const billValueInputAction = value => {
+  return {
+    type: ACTIONS.BILL_VALUE_INPUT,
+    value
+  };
+};
+
+export const tipPercentageInputAction = value => {
+  return {
+    type: ACTIONS.TIP_PERCENTAGE_INPUT,
+    value
+  };
+};
+
+const update = (action, model) => {
+  switch (action.type) {
+    case ACTIONS.BILL_VALUE_INPUT: {
+      if (action.value === "")
+        return { ...model, tip: "", bill: "", total: "" };
+
+      const bill = toFloat(action.value);
+      return calculateTip({ ...model, bill });
+    }
+
+    case ACTIONS.TIP_PERCENTAGE_INPUT: {
+      if (action.value === "")
+        return { ...model, tip: "", tipPercentage: "", total: "" };
+
+      const tipPercentage = toFloat(action.value);
+      return calculateTip({ ...model, tipPercentage });
+    }
+
+    default: {
+      return model;
+    }
+  }
+};
+
+const calculateTip = model => {
+  const { bill, tipPercentage } = model;
+
+  const tip = bill * (tipPercentage / 100);
+  const total = bill + tip;
+
+  console.log(bill, tipPercentage, tip, total);
+  return {
+    bill,
+    tipPercentage,
+    tip,
+    total
+  };
+};
+
+const toFloat = R.pipe(
+  parseFloat,
+  R.defaultTo(0)
+);
 
 export default update;
--- src/View.js
+++ src/View.js
@@ -1,13 +1,42 @@
 import * as R from "ramda";
 import hh from "hyperscript-helpers";
 import { h } from "virtual-dom";
+import { billValueInputAction, tipPercentageInputAction } from "./Update";
 
-const { div, h1, pre } = hh(h);
+const { div, h1, pre, label, input, span } = hh(h);
+
+const inputSet = (dispatch, labelText, value, inputAction) => {
+  return div({ className: "mb3 w-60" }, [
+    label({ className: "db mb1 b f5" }, labelText),
+    input(
+      {
+        className: "pa2 tr db",
+        value: value,
+        oninput: e => dispatch(inputAction(e.target.value))
+      },
+      value
+    )
+  ]);
+};
+
+const labelSet = (labelText, value) =>
+  div({ className: "w-60 mv2" }, [
+    span({ className: "b" }, labelText),
+    span({ className: "fr" }, `$${value}`)
+  ]);
 
 const view = (dispatch, model) => {
   return div({ className: "mw6 center" }, [
     h1({ className: "f2 pv2 bb" }, "Tip Calculator"),
-    pre(JSON.stringify(model, null, 2))
+    inputSet(dispatch, "Bill Amount", model.bill, value =>
+      billValueInputAction(value)
+    ),
+    inputSet(dispatch, "Tip %", model.tipPercentage, value =>
+      tipPercentageInputAction(value)
+    ),
+    labelSet("Tip:", model.tip),
+    labelSet("Total:", model.total),
+    pre(JSON.stringify(model, null, 2)) //Only for development
   ]);
 };
 
